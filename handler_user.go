f004aedae60da51c264363c1af2f310af81a0aa6
philipreese <pbreese42@gmail.com>
2025-06-02 12:46:47 -0400

    add list users command

--- handler_user.go
+++ handler_user.go
@@ -55,6 +55,22 @@ func handlerRegister(s *state, cmd command) error {
 	return nil
 }
 
+func handlerListUsers(s *state, cmd command) error {
+	users, err := s.db.GetUsers(context.Background())
+	if err != nil {
+		return fmt.Errorf("couldn't list users: %w", err)
+	}
+
+	for _, user := range users {
+		output := user.Name
+		if output == s.cfg.CurrentUserName {
+			output += " (current)"
+		}
+		fmt.Printf("* %s\n", output)
+	}
+	return nil
+}
+
 func printUser(user database.User) {
 	fmt.Printf(" * ID:   %v\n", user.ID)
 	fmt.Printf(" * Name: %v\n", user.Name)
